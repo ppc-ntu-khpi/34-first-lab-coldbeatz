97c171fcb47f85d5f81c8b3d4757c7aa7a143141
ПК <@192.168.1.104>
2020-10-09 00:04:11 +0300

    1.3 classes

--- src/Quotation.java
+++ src/Quotation.java
@@ -1,9 +1,10 @@
 
 public class Quotation {
 	
-	private String quote = "Welcome to Sun!";
+	private String quote = "Be yourself; everyone else is already taken.";
+	private String autor = "Oscar Wilde";
 	
 	public void display() {
-		System.out.println(quote);
+		System.out.println(quote + " (" + autor + ")");
 	}
 }
--- src/Shirt.java
+++ src/Shirt.java
@@ -1,10 +1,11 @@
 
 public class Shirt {
 	
-	public int shirtID = 0; // стандартне значення номера моделі сорочки
-	public String description = "-description required-"; // стандартний опис сорочки
-	public char colorCode = 'U'; // R=червоний, B=синій, G=зелений, U=невідомо
-	public double price = 0.0; // стандартна вартість сорочки
+	public int shirtID = 10; // стандартне значення номера моделі сорочки
+	public String description = "default description"; // стандартний опис сорочки
+	public char colorCode = 'u'; // R=червоний, B=синій, G=зелений, U=невідомо
+	public String color = "unset";
+	public double price = 10.5; // стандартна вартість сорочки
 	public int quantityInStock = 0; // стандартна кількість на складі
 	
 	/**
@@ -14,6 +15,7 @@ public class Shirt {
 		System.out.println("Shirt ID: " + shirtID);
 		System.out.println("Shirt description:" + description);
 		System.out.println("Color Code: " + colorCode);
+		System.out.println("Color: " + color);
 		System.out.println("Shirt price: $" + price);
 		System.out.println("Quantity in stock: " + quantityInStock);
 	}
