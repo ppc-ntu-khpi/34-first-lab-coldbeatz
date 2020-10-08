92c6a1ee1449f2f9bfa8b51ee45ad95dc7f0f970
ПК <@192.168.1.104>
2020-10-08 23:07:39 +0300

    Shirt

---
+++ src/Shirt.java
@@ -0,0 +1,20 @@
+
+public class Shirt {
+	
+	public int shirtID = 0; // стандартне значення номера моделі сорочки
+	public String description = "-description required-"; // стандартний опис сорочки
+	public char colorCode = 'U'; // R=червоний, B=синій, G=зелений, U=невідомо
+	public double price = 0.0; // стандартна вартість сорочки
+	public int quantityInStock = 0; // стандартна кількість на складі
+	
+	/**
+	 * виводить всю інформацію про сорочку на екран
+	 */
+	public void displayShirtInformation() {
+		System.out.println("Shirt ID: " + shirtID);
+		System.out.println("Shirt description:" + description);
+		System.out.println("Color Code: " + colorCode);
+		System.out.println("Shirt price: $" + price);
+		System.out.println("Quantity in stock: " + quantityInStock);
+	}
+}
